92f2cad3d9bdd7c6bf152d3894262b984626c9f6
hillardb357 <hillard.49@osu.edu>
2024-12-27 14:16:27 -0500

    commit

--- byta-books/src/App.js
+++ byta-books/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// Components
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import DownloadCTA from './components/DownloadCTA';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <DownloadCTA />
+      </main>
+      <Footer />
     </div>
   );
 }
